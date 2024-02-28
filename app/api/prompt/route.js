4b5fc25c4c8561d04a2de315326f8e9be5f09335
meemimos <sayabcz@hotmail.com>
2024-02-28 12:09:17 +1100

    data fetch issue

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,12 +1,16 @@
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
+import { revalidatePath } from "next/cache";
 
 export const GET = async (request) => {
     try {
         await connectToDB();
 
         const prompts = await Prompt.find({}).populate('creator');
-
+        //To dynamically get the path
+        const path = request.nextUrl.searchParams.get("path") || "/";
+        revalidatePath(path);
+        
         return new Response(JSON.stringify(prompts), { status: 200 })
         
     } catch (error) {
